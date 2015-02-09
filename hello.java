bd8c7315955f2b36e5d42a3e0c9628f5aa30d220
Manoj Mali <manoj13731@gmail.com>
2015-02-09 19:28:07 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+/**
+ * The hello class implements an application that
+ * simply prints "Hello Jimmy!" to standard output.
+ */
+class hello
+ {
+    public static void main (String [] felight) {
+    System.out.println("Hello Jimmy!"); //Display the string
+   
+    }
+  }
